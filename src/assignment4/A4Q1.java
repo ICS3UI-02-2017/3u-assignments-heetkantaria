d55bbdb4622790bf906bbab9652544184aba651f
kanth4624 <kanth4624@HRH-IC0027337.wrdsb.ca>
2018-03-01 11:01:28 -0500

    initialize repository

--- src/assignment4/A4Q1.java
+++ src/assignment4/A4Q1.java
@@ -4,6 +4,8 @@
  */
 package assignment4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kanth4624
@@ -14,6 +16,21 @@ public class A4Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // 
+
+        //use a scanner to read info
+        Scanner input = new Scanner(System.in);
+
+        //create something to answer
+        System.out.println(" Please enter your name ");
+
+        //make a string to store a name
+        String name = input.nextLine();
+
+        //say hello 
+        System.out.print(" hello " + name);
+
+        //ask how are they and on same line as one above
+        System.out.print(". How are you today? ");
+
     }
 }
