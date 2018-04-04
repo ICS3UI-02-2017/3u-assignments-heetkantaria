d9404773a88237228e315214138232b0d7aeca3a
kanth4624 <kanth4624@HRH-IC0027337.wrdsb.ca>
2018-04-04 10:27:47 -0400

    initialize repository

--- src/assignment5/A5Q1.java
+++ src/assignment5/A5Q1.java
@@ -16,57 +16,70 @@ public class A5Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        //repeat the translation
+        //repeat the translator
         while (true) {
 
             // create a scanner
             Scanner input = new Scanner(System.in);
 
             //tell user to enter in a word to translate
-            System.out.println("Enter a word to translate to Ubbi Dubbi: ");
+            System.out.println("Enter a word you would like to translate into Ubbi Dubbi: ");
 
-            //create a string
+            // make a string for the original word
             String orig = input.nextLine();
 
-            //create  string for translation
+            //create a string for the translated word
             String translate = " " + orig;
 
-            //sanitize the input
+            //sanitize the input to all letters lowercase
             orig = orig.toLowerCase();
 
-            //use a loop to go through the characters and look for a vowel
-            for (int abcpos = 0; abcpos < translate.length(); abcpos++) {
+            // check to see if word starts with vowel
+            if (orig.startsWith("a") || orig.startsWith("e") || orig.startsWith("i") || orig.startsWith("o") || orig.startsWith("u")) {
 
-                //check if any of the words contain the vowels
-                //repeated for all vowels
-                if (translate.charAt(abcpos) == 'e' || translate.charAt(abcpos) == 'a' || translate.charAt(abcpos) == 'o' || translate.charAt(abcpos) == 'u' || translate.charAt(abcpos) == 'i') {
-                 
-                    //if two vowels are in a row, it will only put ub infront of the first one
-                    //Repeated for all vowels
-                    if (!(translate.charAt(abcpos - 2) == 'e' || translate.charAt(abcpos - 2) == 'a' || translate.charAt(abcpos - 2) == 'o' || translate.charAt(abcpos - 2) == 'i' || translate.charAt(abcpos - 2) == 'u')) {
+                //stick ub at the front of the word
+                String translate2 = "ub" + orig;
+                System.out.println(orig + " in Ubbi Dubbi is " + translate2);
 
-                        //abcpos is the position of vowel
-                        String start = translate.substring(0, abcpos);
-                        String end = translate.substring(abcpos);
+            } else {
+                //count how many letters in in the word 
+                int length = translate.length();
 
-                        //Create the new Ubbi Dubbi translation word with "ub" in front of vowel
-                        translate = start + "ub" + end;
+                // use a for loop to go through the characters and look for a vowel
+                for (int pos = 0; pos < length; pos++) {
 
-                        //checks letter position for next 3 as well
-                        abcpos = abcpos + 3;
+                    // //check if any of the words contain the vowels
+                    //repeated for all vowels
+                    if (translate.charAt(pos) == 'e' || translate.charAt(pos) == 'a' || translate.charAt(pos) == 'i' || translate.charAt(pos) == 'u' || translate.charAt(pos) == 'o') {
 
-                        //translated word is outputed or computed to Ubbi Dubbi
-                        System.out.println(orig + " is" + translate + " in Ubbi Dubbi.");
+                        //if two vowels are in a row, it will only put ub infront of the first one
+                        //Repeated for all vowels 
 
-                        //done looking so stop the loop
-                        break;
+                        if (!(translate.charAt(pos - 2) == 'e' || translate.charAt(pos - 2) == 'a' || translate.charAt(pos - 2) == 'i' || translate.charAt(pos - 2) == 'u' || translate.charAt(pos - 2) == 'o')) {
 
+                            // adds "ub" infront of the vowel 
+                            String start = translate.substring(0, pos);
+                            String end = translate.substring(pos);
+
+                            //Create the new Ubbi Dubbi translation word with "ub" in front of vowel
+                            translate = start + "ub" + end;
+
+                            // update the length of the translated word
+                            length = translate.length();
+
+                            // Checks letter position of next 3
+                            pos = pos + 3;
+
+                        }
                     }
+
                 }
+
+                System.out.println(orig + " In ubbi dubbi is " + translate + ".");
+
+                //done looking so stop the loop
+                break;
             }
-
         }
-
     }
 }
