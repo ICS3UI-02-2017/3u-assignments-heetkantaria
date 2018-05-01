df9c615473645e11eba35ff8fcfb842f3f668eaf
kanth4624 <kanth4624@HRH-IC0027337.wrdsb.ca>
2018-05-01 11:15:21 -0400

    initialize repository

--- src/assignment7/A7Q7.java
+++ src/assignment7/A7Q7.java
@@ -12,38 +12,42 @@ import java.util.Scanner;
  */
 public class A7Q7 {
 
-     //create a Method to find the first digit of aq number 
+    //create a Method to find the first digit of aq number 
     public static int firstDigit(int num) {
-   
+
         //if number is greater than 9 then it is divided until the first digit left 
-        while(num > 9) {
-          num /= 10;
+        //if first number is positive
+        while (num > 9) {
+            num /= 10;
+        }
+        //If the first number is negative
+        int negativenum = 0;
+        while (num <= -10) {
+            num = num / 10;
+            negativenum = num * -1;
 
-     }
-        
-
-     //return the number
-     return num;
+        }
+        return num;
     }
-            
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         //Create a Scanner
         Scanner input = new Scanner(System.in);
-        
-        //user isasked to enter in a integer
+
+        //user is asked to enter in a integer
         System.out.print("Enter in an integer: ");
-        
+
         //create an integer 
         int number = input.nextInt();
-        
+
         //output the first digit of the integer that was inputed
         System.out.print("The first digit of the integer is " + firstDigit(number) + ". ");
-        
-        
-     
-}
+
+
+
+    }
 }
